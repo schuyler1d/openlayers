schuyler1d/openlayers

commit 8421363db5993a20a376498e9eecc74f39f66b1e

don't allow a baselayer to unselect itself
git-svn-id: http://svn.openlayers.org/trunk@1134 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

openlayers/lib/OpenLayers/Control/LayerSwitcher.js
@@ -121,9 +121,11 @@ OpenLayers.Control.LayerSwitcher.prototype =
             var labelSpan = document.createElement("span");
             labelSpan.innerHTML = layer.name;
             labelSpan.style.verticalAlign = (baseLayer) ? "bottom" : "auto";
-            Event.observe(labelSpan, 
-                          "click", 
-                          this.onLabelClick.bindAsEventListener(inputElem));
+            if (!(baseLayer && checked)) {
+                Event.observe(labelSpan, 
+                              "click", 
+                             this.onLabelClick.bindAsEventListener(inputElem));
+            }
 
             // create line break
             var br = document.createElement("br");

commit 2bc2c5291c99ae97892a18ca2f6257975898e976

fixing bug introduced with r1134
git-svn-id: http://svn.openlayers.org/trunk@1172 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

openlayers/lib/OpenLayers/Control/LayerSwitcher.js
@@ -125,11 +125,9 @@ OpenLayers.Control.LayerSwitcher.prototype =
             var labelSpan = document.createElement("span");
             labelSpan.innerHTML = layer.name;
             labelSpan.style.verticalAlign = (baseLayer) ? "bottom" : "auto";
-            if (!(baseLayer && checked)) {
-                Event.observe(labelSpan, 
-                              "click", 
-                             this.onLabelClick.bindAsEventListener(inputElem));
-            }
+            Event.observe(labelSpan, 
+                          "click", 
+                          this.onLabelClick.bindAsEventListener(inputElem));
 
             // create line break
             var br = document.createElement("br");
@@ -161,9 +159,12 @@ OpenLayers.Control.LayerSwitcher.prototype =
      * @param {Event} e
      */
     onLabelClick: function(e) {
-        this.checked = !this.checked;
+        var clickedRadioButton = ((this.type == "radio") && (this.checked));
+        if (!clickedRadioButton) {
+            this.checked = !this.checked;
+        }
     },
-
+    
     /** Need to update the map accordingly whenever user clicks in either of
      *   the layers.
      * 