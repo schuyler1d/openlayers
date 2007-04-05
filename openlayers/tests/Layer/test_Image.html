schuyler1d/openlayers

commit 571c3511069b0e1d0b0309f5801afc1561f63bad

#529 give tiles gutters - all layers that use Tile.Image must now look after layer.imageSize and layer.imageOffset - this is handled by layer.setTileSize - for untiled layers, setTileSize must be defined by the subclass - gutters are currently supported in Layer.Mapserver and Layer.WMS
git-svn-id: http://svn.openlayers.org/trunk@2979 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

openlayers/lib/OpenLayers/Tile/Image.js
@@ -15,6 +15,13 @@ OpenLayers.Tile.Image.prototype =
     /** @type DOMElement img */
     imgDiv: null,
 
+    /**
+     * The image element is appended to the frame.  Any gutter on the image 
+     * will be hidden behind the frame. 
+     * 
+     * @type DOMElement div */ 
+    frame: null, 
+
     /** 
     * @constructor
     *
@@ -26,6 +33,9 @@ OpenLayers.Tile.Image.prototype =
     */
     initialize: function(layer, position, bounds, url, size) {
         OpenLayers.Tile.prototype.initialize.apply(this, arguments);
+        this.frame = document.createElement('div'); 
+        this.frame.style.overflow = 'hidden'; 
+        this.frame.style.position = 'absolute'; 
     },
 
     /**
@@ -34,12 +44,16 @@ OpenLayers.Tile.Image.prototype =
     destroy: function() {
         if (this.imgDiv != null)  {
             OpenLayers.Event.stopObservingElement(this.imgDiv.id);
-            if (this.imgDiv.parentNode == this.layer.div) {
-                this.layer.div.removeChild(this.imgDiv);
+            if (this.imgDiv.parentNode == this.frame) {
+                this.frame.removeChild(this.imgDiv);
                 this.imgDiv.map = null;
             }
         }
         this.imgDiv = null;
+        if ((this.frame != null) && (this.frame.parentNode == this.layer.div)) { 
+            this.layer.div.removeChild(this.frame); 
+        }
+        this.frame = null; 
         OpenLayers.Tile.prototype.destroy.apply(this, arguments);
     },
 
@@ -60,14 +74,17 @@ OpenLayers.Tile.Image.prototype =
         this.imgDiv.viewRequestID = this.layer.map.viewRequestID;
         
         this.url = this.layer.getURL(this.bounds);
-  
+        // position the frame 
+        OpenLayers.Util.modifyDOMElement(this.frame, 
+                                         null, this.position, this.size);   
+
         if (this.layer.alpha) {
             OpenLayers.Util.modifyAlphaImageDiv(this.imgDiv,
-                    null, this.position, this.size, this.url);
+                    null, null, this.layer.imageSize, this.url);
         } else {
             this.imgDiv.src = this.url;
             OpenLayers.Util.modifyDOMElement(this.imgDiv,
-                    null, this.position, this.size) ;
+                    null, null, this.layer.imageSize) ;
         }
         this.drawn = true;
         return true;
@@ -102,20 +119,20 @@ OpenLayers.Tile.Image.prototype =
     initImgDiv: function() {
         if (this.layer.alpha) {
             this.imgDiv = OpenLayers.Util.createAlphaImageDiv(null,
-                                                           this.position,
-                                                           this.size,
+                                                           this.layer.imageOffset,
+                                                           this.layer.imageSize,
                                                            null,
-                                                           "absolute",
+                                                           "relative",
                                                            null,
                                                            null,
                                                            null,
                                                            true);
         } else {
             this.imgDiv = OpenLayers.Util.createImage(null,
-                                                      this.position,
-                                                      this.size,
+                                                      this.layer.imageOffset,
+                                                      this.layer.imageSize,
                                                       null,
-                                                      "absolute",
+                                                      "relative",
                                                       null,
                                                       null,
                                                       true);
@@ -131,7 +148,9 @@ OpenLayers.Tile.Image.prototype =
         OpenLayers.Event.observe( this.imgDiv, "load",
                         this.checkImgURL.bindAsEventListener(this) );
         */
-        this.layer.div.appendChild(this.imgDiv);
+        this.frame.appendChild(this.imgDiv); 
+        this.layer.div.appendChild(this.frame); 
+
         if(this.layer.opacity != null) {
             
             OpenLayers.Util.modifyDOMElement(this.imgDiv, null, null, null,

openlayers/tests/Tile/test_Image.html
@@ -29,12 +29,15 @@
 
         var map = new OpenLayers.Map('map');
         
-        layer = new OpenLayers.Layer.WMS("Name", "http://labs.metacarta.com/TESTURL");  
+        var size = new OpenLayers.Size(5,6);
+        layer = new OpenLayers.Layer.WMS("Name",
+                                         "http://labs.metacarta.com/TESTURL",
+                                         null,
+                                         {tileSize: size});  
         map.addLayer(layer);  
         var position = new OpenLayers.Pixel(20,30);
         var bounds = new OpenLayers.Bounds(1,2,3,4);
         var url = "http://www.openlayers.org/dev/tests/tileimage";
-        var size = new OpenLayers.Size(5,6);
         tile = new OpenLayers.Tile.Image(layer, position, bounds, url, size);
         
         tile.draw();
@@ -53,7 +56,7 @@
             REQUEST: "GetMap", STYLES: "",
             EXCEPTIONS: "application/vnd.ogc.se_inimage", FORMAT: "image/jpeg",
             SRS: "EPSG:4326", BBOX: "1,2,3,4",
-            WIDTH: "256", HEIGHT: "256"
+            WIDTH: String(size.w), HEIGHT: String(size.h)
         };
         t.eq( img.src,
              "http://labs.metacarta.com/TESTURL?" + OpenLayers.Util.getParameterString(tParams),
@@ -167,11 +170,57 @@
         t.delay_call( 1, function() { t.eq(tile.imgDiv, null, "Alpha tile imgDiv is null.") } );
         
     }
+
+    function test_05_Tile_Image_gutters(t) {
+        t.plan(5);
+        
+        var gutter = 0;
+        var name = 'Test Layer';
+        var url = "http://octo.metacarta.com/cgi-bin/mapserv";
+        var params = { map: '/mapdata/vmap_wms.map', 
+                       layers: 'basic', 
+                       format: 'image/png'};
+
+
+        var map = new OpenLayers.Map('map');
+        var layer = new OpenLayers.Layer.WMS(name, url, params, {gutter: gutter});
+        map.addLayer(layer);
+        map.setCenter(new OpenLayers.LonLat(0,0), 5);
+        
+        var tile = layer.grid[0][0];
+        
+        t.ok(tile.layer.imageSize.equals(tile.size),
+             "zero size gutter doesn't change image size"); 
+
+        t.ok(tile.layer.imageOffset.equals(new OpenLayers.Pixel(0, 0)),
+             "zero size gutter doesn't affect image offset");
+
+        map.destroy();
+        
+        var gutter = 15;
+        var map = new OpenLayers.Map('map');
+        var layer = new OpenLayers.Layer.WMS(name, url, params, {gutter: gutter});
+        map.addLayer(layer);
+        map.setCenter(new OpenLayers.LonLat(0,0), 5);
+        var tile = layer.grid[0][0];
+        t.ok(tile.layer.imageSize.equals(new OpenLayers.Size(tile.size.w + (2 * gutter),
+                                                             tile.size.h + (2 * gutter))),
+             "gutter properly changes image size"); 
+
+        t.ok(tile.layer.imageOffset.equals(new OpenLayers.Pixel(-gutter, -gutter)),
+             "gutter properly sets image offset");
+
+        t.ok(tile.bounds.equals(new OpenLayers.Bounds(-33.75, 33.75, -22.5, 45)),
+             "gutter doesn't affect tile bounds");
+
+        map.destroy();
+    }
+    
   // -->
   </script>
 </head>
 <body>
-<div id="map" style="height:500px;width:500px"></div>
+<div id="map" style="height:550px;width:500px"></div>
 </body>
 </html>
 

openlayers/examples/gutter.html
@@ -1,0 +1,49 @@
+<html> 
+  <head> 
+    <style type="text/css"> 
+        #map { 
+            width: 500px; 
+            height: 300px; 
+            border: 1px solid gray; 
+        } 
+        p.caption { 
+            width: 500px; 
+        } 
+    </style> 
+    <script src="../lib/OpenLayers.js"></script> 
+    <script type="text/javascript"> 
+        <!-- 
+        OpenLayers.IMAGE_RELOAD_ATTEMPTS = 2; 
+        var map; 
+        window.onload = function() { 
+            options = {maxExtent: new OpenLayers.Bounds(-73.5295, 41.2318, 
+                                                        -69.9097, 42.8883), 
+                       maxResolution: 0.0003} 
+            map = new OpenLayers.Map('map', options); 
+            var roads15 = new OpenLayers.Layer.WMS( "Roads (15px gutter)",  
+                "http://boston.freemap.in/cgi-bin/mapserv?map=/www/freemap.in/boston/map/gmaps.map&", 
+                {layers: 'roads_200_40'}, 
+                {gutter: 15});  
+            var roads = new OpenLayers.Layer.WMS( "Roads (no gutter)",  
+                "http://boston.freemap.in/cgi-bin/mapserv?map=/www/freemap.in/boston/map/gmaps.map&", 
+                {layers: 'roads_200_40'});  
+            map.addLayers([roads, roads15]); 
+            map.addControl(new OpenLayers.Control.LayerSwitcher()); 
+            map.setCenter(new OpenLayers.LonLat(-71.848, 42.2), 0); 
+        } 
+        // --> 
+    </script> 
+  </head> 
+  <body> 
+    <h1>OpenLayers Gutter Example</h1> 
+    <div id="map"></div> 
+    <p class="caption"> 
+        When you render tiles with certain types of symbols, there are artifacts 
+        at tile edges that make symbology not look continuous.  In the center of 
+        the above map (when it first loads), the large orange road is split 
+        vertically by a tile.  Open the layer switcher and change to the layer 
+        with a 15 pixel gutter to see how the symbology looks nicer. 
+    </p> 
+         
+  </body> 
+</html>

openlayers/examples/mapserver.html
@@ -18,7 +18,8 @@
         function init(){
             map = new OpenLayers.Map( 'map' );
             layer = new OpenLayers.Layer.MapServer( "OpenLayers WMS", 
-                    "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'} );
+                    "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'},
+                    {gutter: 15});
             map.addLayer(layer);
 
             map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);

openlayers/examples/wms-untiled.html
@@ -8,19 +8,27 @@
         }
     </style>
     <script src="../lib/OpenLayers.js"></script>
-    <script src="../lib/OpenLayers/Layer/MapServer/Untiled.js"></script>
     <script type="text/javascript">
-        window.onload = function(){
-            var map = new OpenLayers.Map( 'map', {maxResolution: 'auto'} );
-            var layer = new OpenLayers.Layer.MapServer.Untiled( "MapServer Untiled", 
+        <!--
+        var lon = 5;
+        var lat = 40;
+        var zoom = 5;
+        var map, layer;
+
+        function init(){
+            map = new OpenLayers.Map( 'map' );
+            layer = new OpenLayers.Layer.WMS.Untiled( "OpenLayers WMS", 
                     "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'} );
             map.addLayer(layer);
-            map.setCenter(new OpenLayers.LonLat(0, 0), 1);
+
+            map.setCenter(new OpenLayers.LonLat(lon, lat), zoom);
             map.addControl( new OpenLayers.Control.LayerSwitcher() );
         }
+        
+        // -->
     </script>
   </head>
-  <body>
+  <body onload="init()">
     <div id="map"></div>
   </body>
 </html>

openlayers/lib/OpenLayers/Layer.js
@@ -65,11 +65,44 @@ OpenLayers.Layer.prototype = {
      * @type Boolean
      */
     inRange: false,
+    
+    /**
+     * For layers that use Tile.Image, the image size is cached here.  For
+     * layers without a gutter, the image size is equal to the tile size.
+     * For layers with a gutter, the image is larger than the tile by twice
+     * the gutter in each dimension.
+     * 
+     * @type OpenLayers.Size
+     * @private
+     */
+    imageSize: null,
+    
+    /**
+     * For layers that use Tile.Image, the image offset is cached here.
+     * Layers without a gutter have zero offset.  For layers with a gutter,
+     * the image offset represents displacement due to the gutter.
+     * 
+     * @type OpenLayers.Pixel
+     * @private
+     */
+    imageOffset: null,
 
   // OPTIONS
 
     /** @type Array */
     options: null,
+
+    /** Determines the width (in pixels) of the gutter around image tiles 
+     * to ignore.  By setting this property to a non-zero value, images 
+     * will be requested that are wider and taller than the tile size by 
+     * a value of 2 x gutter.  This allows artifacts of rendering at tile 
+     * edges to be ignored.  Set a gutter value that is equal to half the size 
+     * of the widest symbol that needs to be displayed.  Defaults to zero.
+     * Non-tiled layers always have zero gutter.
+     *  
+     * @type Int 
+    */ 
+    gutter: 0, 
 
     /** @type String */
     projection: null,    
@@ -243,9 +276,41 @@ OpenLayers.Layer.prototype = {
         
         if (!this.isBaseLayer) {
             this.inRange = this.calculateInRange();
-        }    
+        }
+        
+        // deal with gutters
+        this.setTileSize();
     },
   
+    /**
+     * Set the tile size based on the map size.  This also sets layer.imageSize
+     * and layer.imageOffset for use by Tile.Image.
+     *
+     * @param OpenLayers.Size
+     */
+    setTileSize: function(size) {
+        var tileSize = (size) ? size :
+                                ((this.tileSize) ? this.tileSize :
+                                                   this.map.getTileSize());
+        this.tileSize = tileSize;
+        if(this.gutter) {
+            // layers with gutters need non-null tile sizes
+            //if(tileSize == null) {
+            //    OpenLayers.console.error("Error in layer.setMap() for " +
+            //                              this.name + ": layers with gutters " +
+            //                              "need non-null tile sizes");
+            //}
+            this.imageOffset = new OpenLayers.Pixel(-this.gutter, -this.gutter); 
+            this.imageSize = new OpenLayers.Size(tileSize.w + (2 * this.gutter), 
+                                                 tileSize.h + (2 * this.gutter)); 
+        } else {
+            // layers without gutters may have null tile size - as long
+            // as they don't rely on Tile.Image
+            this.imageSize = tileSize;
+            this.imageOffset = new OpenLayers.Pixel(0, 0);
+        }
+    },
+
     /**
     * @returns Whether or not the layer should be displayed (if in range)
     * @type Boolean
@@ -560,13 +625,30 @@ OpenLayers.Layer.prototype = {
     },
     
     /**
+     * Adjusts the extent of a bounds in map units by the layer's gutter
+     * in pixels.
+     * 
+     * @param {OpenLayers.Bounds} bounds
+     * @type OpenLayers.Bounds
+     * @return A bounds adjusted in height and width by the gutter
+     */
+    adjustBoundsByGutter: function(bounds) {
+        var mapGutter = this.gutter * this.map.getResolution();
+        bounds = new OpenLayers.Bounds(bounds.left - mapGutter,
+                                       bounds.bottom - mapGutter,
+                                       bounds.right + mapGutter,
+                                       bounds.top + mapGutter);
+        return bounds;
+    },
+    
+    /**
      * Sets the opacity for the entire layer (all images)
      * @param {Float} opacity
      */
     setOpacity: function(opacity) {
         this.opacity = opacity;
         for(var i=0; i<this.div.childNodes.length; ++i) {
-            var element = this.div.childNodes[i];
+            var element = this.div.childNodes[i].firstChild;
             OpenLayers.Util.modifyDOMElement(element, null, null, null, 
                                              null, null, null, opacity);
         }

openlayers/lib/OpenLayers/Layer/MapServer.js
@@ -87,7 +87,9 @@ OpenLayers.Layer.MapServer.prototype =
      * @type String
      */
     getURL: function (bounds) {
-        
+        if(this.gutter) {
+            bounds = this.adjustBoundsByGutter(bounds);
+        }
         // Make a list, so that getFullRequestString uses literal "," 
         var extent = [bounds.left, bounds. bottom, bounds.right, bounds.top];
         
@@ -95,10 +97,10 @@ OpenLayers.Layer.MapServer.prototype =
         var url = this.getFullRequestString(
                      {mapext:   extent,
                       imgext:   extent,
-                      map_size: [this.tileSize.w,this.tileSize.h],
-                      imgx:     this.tileSize.w/2,
-                      imgy:     this.tileSize.h/2,
-                      imgxy:    [this.tileSize.w,this.tileSize.h]
+                      map_size: [this.imageSize.w, this.imageSize.h],
+                      imgx:     this.imageSize.w / 2,
+                      imgy:     this.imageSize.h / 2,
+                      imgxy:    [this.imageSize.w, this.imageSize.h]
                       });
         
         return url;

openlayers/lib/OpenLayers/Layer/MapServer/Untiled.js
@@ -99,11 +99,20 @@ OpenLayers.Layer.MapServer.Untiled.prototype =
      * @param {OpenLayers.Map} map
      */
     setMap: function(map) {
-        //determine new tile size
-        this.tileSize = map.getSize();
-        this.tileSize.w = this.tileSize.w * this.ratio;
-        this.tileSize.h = this.tileSize.h * this.ratio;
         OpenLayers.Layer.HTTPRequest.prototype.setMap.apply(this, arguments);
+    },
+    
+    /**
+     * Set the tile size based on the map size.  This also sets layer.imageSize
+     * and layer.imageOffset for use by Tile.Image.
+     */
+    setTileSize: function() {
+        var tileSize = this.map.getSize();
+        tileSize.w = tileSize.w * this.ratio;
+        tileSize.h = tileSize.h * this.ratio;
+        this.tileSize = tileSize;
+        this.imageSize = tileSize;
+        this.imageOffset = new OpenLayers.Pixel(0, 0);
     },
 
     /** When it is not a dragging move (ie when done dragging)
@@ -149,9 +158,7 @@ OpenLayers.Layer.MapServer.Untiled.prototype =
                                       center.lat + (tileHeight / 2));
 
             //determine new tile size
-            this.tileSize = this.map.getSize();
-            this.tileSize.w = this.tileSize.w * this.ratio;
-            this.tileSize.h = this.tileSize.h * this.ratio;
+            this.setTileSize();
 
             //formulate request url string
             var url = this.getURL(tileBounds); 

commit 3c5c4a020a38adac308817c9975882689ebc517b

#632: fixing the image layer that broke with r2979 - layer now sets its tileSize and imageSize appropriately - this also addesses (dup) ticket 511
git-svn-id: http://svn.openlayers.org/trunk@3020 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

openlayers/examples/image-layer.html
@@ -13,10 +13,11 @@
     <script src="../lib/OpenLayers.js"></script>
     <script type="text/javascript">
         <!--
+        var map;
         function init(){
-            var map = new OpenLayers.Map('map');
+            map = new OpenLayers.Map('map');
 
-            var options = {maxResolution: 'auto', numZoomLevels: 3};
+            var options = {numZoomLevels: 3};
             
             var graphic = new OpenLayers.Layer.Image(
                                 'City Lights',

openlayers/lib/OpenLayers/Layer/Image.js
@@ -24,17 +24,23 @@ OpenLayers.Layer.Image.prototype =
     /** @type String */
     url: null,
 
-    /** @type OpenLayers.Bounds */
+    /**
+     * The image bounds in map units
+     * @type OpenLayers.Bounds
+     */
     extent: null,
     
-    /** @type OpenLayers.Size */
+    /**
+     * The image size in pixels
+     * @type OpenLayers.Size
+     */
     size: null,
 
     /** @type OpenLayers.Tile.Image */
     tile: null,
 
-    /** The ratio of height/width represented by a single pixel in the graphic
-     * 
+    /**
+     * The ratio of height/width represented by a single pixel in the graphic
      * @type Float */
     aspectRatio: null,
 
@@ -94,10 +100,16 @@ OpenLayers.Layer.Image.prototype =
      * @param {OpenLayers.Map} map
      */
     setMap: function(map) {
-        // If nothing to do with resolutions has been set, assume a single
-        //  resolution determined by extent/size
+        /**
+         * If nothing to do with resolutions has been set, assume a single
+         * resolution determined by ratio*extent/size - if an image has a
+         * pixel aspect ratio different than one (as calculated above), the
+         * image will be stretched in one dimension only.
+         */
         if( this.options.maxResolution == null ) {
-            this.options.maxResolution = this.extent.getWidth() / this.size.w;
+            this.options.maxResolution = this.aspectRatio *
+                                         this.extent.getWidth() /
+                                         this.size.w;
         }
         OpenLayers.Layer.prototype.setMap.apply(this, arguments);
     },
@@ -116,11 +128,8 @@ OpenLayers.Layer.Image.prototype =
         if(zoomChanged || firstRendering) {
 
             //determine new tile size
-            var tileWidth = this.extent.getWidth() / this.map.getResolution();
-            var tileHeight = this.extent.getHeight() /
-                             (this.map.getResolution() * this.aspectRatio);
-            var tileSize = new OpenLayers.Size(tileWidth, tileHeight);
-            
+            this.setTileSize();
+
             //determine new position (upper left corner of new bounds)
             var ul = new OpenLayers.LonLat(this.extent.left, this.extent.top);
             var ulPx = this.map.getLayerPxFromLonLat(ul);
@@ -128,16 +137,28 @@ OpenLayers.Layer.Image.prototype =
             if(firstRendering) {
                 //create the new tile
                 this.tile = new OpenLayers.Tile.Image(this, ulPx, this.extent, 
-                                                      this.url, tileSize);
+                                                      this.url, this.tileSize);
             } else {
                 //just resize the tile and set it's new position
-                this.tile.size = tileSize.clone();
+                this.tile.size = this.tileSize.clone();
                 this.tile.position = ulPx.clone();
             }
             this.tile.draw();
         }
     }, 
-    
+
+    /**
+     * Set the tile size based on the map size.  This also sets layer.imageSize
+     * and layer.imageOffset for use by Tile.Image.
+     */
+    setTileSize: function() {
+        var tileWidth = this.extent.getWidth() / this.map.getResolution();
+        var tileHeight = this.extent.getHeight() / this.map.getResolution();
+        this.tileSize = new OpenLayers.Size(tileWidth, tileHeight);
+        this.imageSize = this.tileSize;
+        this.imageOffset = new OpenLayers.Pixel(0, 0);
+    },
+
     /**
      * @param {String} newUrl
      */

openlayers/tests/Layer/test_Image.html
@@ -18,7 +18,7 @@
 
         t.eq( layer.name, "Test Layer", "layer.name is correct" );
         t.ok( layer.id != null, "Layer is given an id");
-        t.ok( layer.projection, "none", "default layer projection correctly set");
+        t.eq( layer.projection, "none", "default layer projection correctly set");
         t.ok( ((layer.chicken == 151) && (layer.foo == "bar")), "layer.options correctly set to Layer Object" );
         t.ok( ((layer.options["chicken"] == 151) && (layer.options["foo"] == "bar")), "layer.options correctly backed up" );
 
@@ -39,7 +39,7 @@
     }
 
     function test_50_Layer_Image_tileTests (t) {
-        t.plan(4);
+        t.plan(9);
         var map = new OpenLayers.Map('map');
         
         layer = new OpenLayers.Layer.Image('Test Layer', 
@@ -49,8 +49,20 @@
 
         map.addLayer(layer);
         map.zoomToMaxExtent();
-        t.eq(layer.tile.position.x,-40, "Tile x positioned correctly at maxextent");
-        t.eq(layer.tile.position.y,107, "Tile y positioned correctly at maxextent");
+        t.ok(layer.imageSize, "layer.imageSize is set");
+        t.ok(layer.tileSize, "layer.tileSize is set");
+        t.ok(layer.tileSize.equals(layer.imageSize), "tileSize equals imageSize");
+
+        // no resolution info was sent, so maxResolution should be calculated
+        // by aspectRatio*extent/size (this is the pixel aspect ratio)
+        var aspectRatio =  (layer.extent.getHeight() / layer.size.h) /
+                           (layer.extent.getWidth() / layer.size.w);
+        t.eq(aspectRatio, layer.aspectRatio, "aspectRatio is properly set");
+        var maxExtent = aspectRatio * layer.extent.getWidth() / layer.size.w;
+        t.eq(maxExtent, layer.maxResolution, "maxResolution is properly set");
+
+        t.eq(layer.tile.position.x,-42, "Tile x positioned correctly at maxextent");
+        t.eq(layer.tile.position.y,106, "Tile y positioned correctly at maxextent");
         t.eq(layer.tile.imgDiv.src, "http://earthtrends.wri.org/images/maps/4_m_citylights_lg.gif", "URL is correct");
         map.zoomIn();
         t.eq(layer.tile.imgDiv.src, "http://earthtrends.wri.org/images/maps/4_m_citylights_lg.gif", "URL is correct");